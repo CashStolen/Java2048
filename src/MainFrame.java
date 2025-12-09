cf6b346614783ba74322d15c8663db3418fda83d
Federeo Trim <234888434+CashStolen@users.noreply.github.com>
2025-12-09 11:19:45 +0800

    Update MainFrame.java

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -7,17 +7,19 @@ import java.awt.event.KeyListener;
 import java.util.Random;
 
 public class MainFrame extends JFrame implements KeyListener{
-    //本身对象继承自JFrame类！！！
-    //构造方法（创建对象的时候执行）
+    //JFrame class
+    //structure
+
+    //try in the psvm(nothing to do with procedure.java)
     public static void main(String[] args) {
-    //初始数组设置
+    //initialize the array
     int[][]arr={
         {0,2,4,8},
         {8,16,16,64},
         {2,4,4,2},
         {4,4,8,0}
         };
-    //把0右移和把数据左移合并
+    //emerge 0
     for(int i=0;i<arr.length;i++){
         int[]newArr=new int[4];
         int index=0;
@@ -29,7 +31,7 @@ public class MainFrame extends JFrame implements KeyListener{
         }
         arr[i]=newArr;
     //System.out.println(arr[0][2]);
-    //数据左移完毕，接下来进行合并、补零
+    //emerge and add 0
         for(int k=0;k<arr.length-1;k++){
             if(arr[i][k]==arr[i][k+1]){
                 arr[i][k]*=2;
@@ -41,7 +43,7 @@ public class MainFrame extends JFrame implements KeyListener{
         }
     
     }
-    for(int i=0;i<arr.length;i++){//二维数组的外循环得用整体的行数来表示！！！
+    for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr[i].length;j++){
             System.out.print(arr[i][j]+" ");
         }
@@ -52,14 +54,14 @@ public class MainFrame extends JFrame implements KeyListener{
     int[][] arr=new int[4][4];
     int Lose=0;
     int score=0;
-    //构造方法
+    //structure
     public MainFrame(){
         //Init_arr();
         //init_22_arr();
         init_arr();
         initFrame();
         paintFrame();
-        //添加键盘监听
+        //addkeylistener
         this.addKeyListener(this);
         setVisible(true);
     }
@@ -97,6 +99,8 @@ public class MainFrame extends JFrame implements KeyListener{
 
         getContentPane().repaint();
     }
+
+    //override all the methods
     
     @Override
     public void keyPressed(KeyEvent e) {
@@ -139,7 +143,7 @@ public class MainFrame extends JFrame implements KeyListener{
     public void keyTyped(KeyEvent e) {
     }
 
-    //左移动
+    //move-left
     public void Move_Left(int act){
         for(int i=0;i<arr.length;i++){
         int[]newArr=new int[4];
@@ -152,7 +156,7 @@ public class MainFrame extends JFrame implements KeyListener{
         }
         arr[i]=newArr;
     //System.out.println(arr[0][2]);
-    //数据左移完毕，接下来进行合并、补零
+    //emerge and add 0
         for(int k=0;k<arr.length-1;k++){
             if(arr[i][k]==arr[i][k+1]){
                 arr[i][k]*=2;
@@ -169,7 +173,7 @@ public class MainFrame extends JFrame implements KeyListener{
     }
     //paintFrame();
     }
-    //反转行
+    //reverse
     public void Rev_arr(){
         for(int i=0;i<4;i++){
             int []newarr=new int[4];
@@ -180,14 +184,14 @@ public class MainFrame extends JFrame implements KeyListener{
         }
 
     }
-    //右移动
+    //move-right
     public void Move_Right(int a){
         Rev_arr();
         Move_Left(a);
         Rev_arr();
         //paintFrame();
     }
-    //顺时针旋转
+    //rotate-clockwise
     public void Rotate_clk(){
         int[][]Newarr=new int[4][4];
         for(int i=0;i<4;i++){
@@ -197,7 +201,7 @@ public class MainFrame extends JFrame implements KeyListener{
         }
         arr=Newarr;
     }
-    //逆时针旋转
+    //rotate-clockwise reversely
     public void Rotate_anticlk(){
         int[][]Newarr=new int[4][4];
         for(int i=0;i<4;i++){
@@ -207,7 +211,7 @@ public class MainFrame extends JFrame implements KeyListener{
         }
         arr=Newarr;
     }
-    //数组拷贝
+    //copy the array
     public void copyArr(int [][]arr_1,int [][]arr_2){
         //arr_1=new int[4][4];
         //arr_2=new int[4][4];
@@ -217,7 +221,7 @@ public class MainFrame extends JFrame implements KeyListener{
             }
         }
     }
-    //失败判定
+    //if you lose
     public void check(){
         if(checkD()==false & checkL()==false & checkR()==false & checkU()==false){
             Lose=1;
@@ -226,7 +230,7 @@ public class MainFrame extends JFrame implements KeyListener{
             Lose=0;
         }
     }
-    //左移动检查
+    //check wheather you can execute move-left
     public boolean checkL(){
         int[][]newArray=new int[4][4];
         copyArr(arr, newArray);
@@ -243,7 +247,7 @@ public class MainFrame extends JFrame implements KeyListener{
         copyArr(newArray, arr);
         return flag;  
     }
-    //右移动检查
+    //check wheather you can execute move-right
     public boolean checkR(){
         int[][]newArray=new int[4][4];
         copyArr(arr, newArray);
@@ -253,14 +257,14 @@ public class MainFrame extends JFrame implements KeyListener{
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j]!=newArray[i][j]){
                     flag=true;
-                    break wai;//命名以跳出外层循环
+                    break wai;
                 }
             }
         }
         copyArr(newArray, arr);
         return flag;  
     }
-    //上移动检查
+    //check wheather you can execute move-up
     public boolean checkU(){
         int[][]newArray=new int[4][4];
         copyArr(arr, newArray);
@@ -272,14 +276,14 @@ public class MainFrame extends JFrame implements KeyListener{
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j]!=newArray[i][j]){
                     flag=true;
-                    break wai;//命名以跳出外层循环
+                    break wai;
                 }
             }
         }
         copyArr(newArray, arr);
         return flag;  
     }
-    //下移动检查
+    //check wheather you can execute move-down
     public boolean checkD(){
         int[][]newArray=new int[4][4];
         copyArr(arr, newArray);
@@ -291,14 +295,14 @@ public class MainFrame extends JFrame implements KeyListener{
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j]!=newArray[i][j]){
                     flag=true;
-                    break wai;//命名以跳出外层循环
+                    break wai;
                 }
             }
         }
         copyArr(newArray, arr);
         return flag;  
     }
-    //随机产生2
+    //randomly generate the initial array
     public void numRandom(){
         int index=0;
         int[]array_1={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
@@ -318,7 +322,7 @@ public class MainFrame extends JFrame implements KeyListener{
             arr[array_1[num]][array_2[num]]=2;
         }
     }
-    //初始化(所有数都有可能)
+    //init_1
     public void Init_arr(){
         int []Arr={0,2,4,8,16,32,64,128,256,512,1024,2048};
         Random random=new Random();
@@ -329,7 +333,7 @@ public class MainFrame extends JFrame implements KeyListener{
             } 
         }
     }
-    //2初始化
+    //init_2
     public void init_arr(){
         int []Arr={2,2,0,0,0,0,0,0,0,0,0,0};
         Random random=new Random();
@@ -340,7 +344,7 @@ public class MainFrame extends JFrame implements KeyListener{
             } 
         }
     }
-    //2初始化变体
+    //init_3
     public void init_22_arr(){
         numRandom();
         numRandom();
